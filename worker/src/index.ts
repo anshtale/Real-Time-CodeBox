0b151a92ca82bb085ca2a1f8635e02576c84ab88
Ansh Tale <anshtale2004@gmail.com>
2025-05-29 00:53:42 +0530

    -added other language support -updated docker commands for accepting input and exec user's code -added cleanup function to delete user related files as soon as exceution is completed -worker almost completed

--- worker/src/index.ts
+++ worker/src/index.ts
@@ -1,55 +1,117 @@
-import {createClient} from 'redis';
-import {exec} from 'child_process'
-import fs from 'fs'
+import { createClient } from 'redis';
+import { exec } from 'child_process'
+import fs from 'fs/promises'
 import path from 'path'
-
 
 const client = createClient();
 const pubClient = createClient();
 
-async function processSubmission(submission:any){
-    const { code, language, userId, submissionId } = JSON.parse(submission);
+async function processSubmission(submission: any) {
+    const { code, language, userId, submissionId, input } = JSON.parse(submission);
 
-    console.log(`Processing submission ${submissionId}`); 
+    console.log(`Processing submission ${submissionId}`);
 
     const codeDir = path.resolve(`./tmp/user-${Date.now()}`);
-    fs.mkdirSync(codeDir,{recursive:true});
+    await fs.mkdir(codeDir, { recursive: true });
 
-    const fileExtension = language == 'python' ? 'py' : 'js';
-    const codeFilePath = path.join(codeDir,`userCode.${fileExtension}`)
 
-    fs.writeFileSync(codeFilePath,code);
+    let codeFilePath = "";
+    let dockerCommand = "";
 
-    const dockerCommand = language === 'python' ? `docker run --rm -v "${codeDir.replace(/\\/g, '/') }:/usr/src/app" python:3.9 python /usr/src/app/userCode.py`
-    : `docker run --rm -v "${codeDir.replace(/\\/g, '/') }:/usr/src/app" node node /usr/src/app/userCode.js`;
+    const inputFilePath = path.join(codeDir, "input.txt");
 
-    exec(dockerCommand, async(error,stdout,stderr)=>{
+
+    try {
+        await fs.writeFile(inputFilePath, input, "utf8");
+
+        switch (language) {
+            case "javascript":
+                codeFilePath = path.join(codeDir, "userCode.js");
+                await fs.writeFile(codeFilePath, code);
+
+                dockerCommand = `docker run --rm --memory="256m" --cpus="1.0" --pids-limit 100 -v "${codeDir.replace(
+                    /\\/g,
+                    "/"
+                )}:/usr/src/app" node:18 sh -c "node /usr/src/app/${path.basename(
+                    codeFilePath
+                )} /usr/src/app/input.txt"`;
+                break;
+
+            case "python":
+                codeFilePath = path.join(codeDir, "userCode.py");
+
+                await fs.writeFile(codeFilePath, code);
+
+                dockerCommand = `docker run --rm --memory="256m" --cpus="1.0" --pids-limit 100 -v "${codeDir.replace(
+                    /\\/g,
+                    "/"
+                )}:/usr/src/app" python:3.9 sh -c "python /usr/src/app/${path.basename(
+                    codeFilePath
+                )} /usr/src/app/input.txt"`;
+                break;
+
+            case "cpp":
+                codeFilePath = path.join(codeDir, "userCode.cpp");
+                await fs.writeFile(codeFilePath, code);
+                dockerCommand = `docker run --rm --memory="256m" --cpus="1.0" --pids-limit 100 \
+-v "${codeDir.replace(/\\/g, "/")}:/usr/src/app" gcc:11  \
+sh -c "g++ /usr/src/app/userCode.cpp -o /usr/src/app/a.out && /usr/src/app/a.out < /usr/src/app/input.txt"`;
+
+                break;
+
+            case "go":
+                codeFilePath = path.join(codeDir, "userCode.go");
+                await fs.writeFile(codeFilePath, code);
+                dockerCommand = `docker run --rm --memory="256m" --cpus="1.0" --pids-limit 100 -v "${codeDir}:/usr/src/app" golang:1.18 sh -c "go run /usr/src/app/userCode.go < /usr/src/app/input.txt"`;
+                break;
+                
+            default: throw new Error("Unsupported language");     
+        }
+    }catch(e){
+        console.error("Failed to prepare code file or Docker command",e);
+        return;
+    }
+
+    exec(dockerCommand,async(error,stdout,stderr)=>{
         let result = stdout || stderr;
-
         if(error){
             result = `Error: ${error.message}`;
         }
 
-        await pubClient.publish(submissionId,result);
-        fs.rmSync(codeDir,{recursive:true,force:true});
-    });
+        console.log(`Result for user ${userId} : ${result}`);
+
+        try{
+            await pubClient.publish(userId,result);
+        }catch(e){
+            console.error("Failed to publish result to Redis,", e)
+        }
+
+        try{
+            await fs.rm(codeDir,{recursive:true,force:true});
+        }catch(cleanupError){
+            console.error("Failed to clean up directory:", cleanupError);
+        }
+    })
 }
 
-async function main(){
-    try{
+async function main() {
+    try {
         await client.connect();
         await pubClient.connect();
 
         console.log("Redis Client Connected");
 
-        while(true){
-            const submission = await client.brPop('problems',0);
+        while (true) {
+            const submission = await client.brPop("problems", 0);
 
             console.log("Processing submission...");
-            await processSubmission(submission?.element)
+
+            if(submission){
+                await processSubmission(submission.element);
+            }
         }
-    }catch(error){
-        console.log("Failed to connect to Redis",error);
+    } catch (error) {
+        console.error("Failed to connect to Redis", error);
     }
 }
 
