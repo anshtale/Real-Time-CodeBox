93b52d317d241c74a343aa16de64ae8065d72a5b
Ansh Tale <anshtale2004@gmail.com>
2025-05-28 11:08:03 +0530

    -fixed ui issues, added proper router with protectedRoutes redirect

--- frontend/src/middleware/ProtectedRouter.tsx
+++ frontend/src/middleware/ProtectedRouter.tsx
@@ -2,18 +2,17 @@ import { useRecoilValue } from "recoil";
 import { userAtom } from "../atoms/userAtom";
 import {  useNavigate } from "react-router-dom";
 
-type ProtectedRouterProps = {
-  children: React.ReactNode;
-};
 
-const ProtectedRouter = ({ children }: ProtectedRouterProps) => {
+const ProtectedRouter = ( {children} : {children : React.ReactNode}) => {
     const user = useRecoilValue(userAtom);
 
     const navigate = useNavigate();
 
-    return (
-        user.id != "" ? children : navigate('/register')
-    )
+    if(!user.id){
+        navigate('/register')
+    }
+
+    return <>{children}</>
 };
 
 export default ProtectedRouter;
