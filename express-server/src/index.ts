793c6b5e018c9a4682510449bd5c1c7c89154ed3
Ansh Tale <anshtale2004@gmail.com>
2025-05-28 10:08:34 +0530

    -updated code structure and removed ws server from primary express-server

---
+++ express-server/src/index.ts
@@ -0,0 +1,50 @@
+import express from "express";
+import { createClient } from "redis";
+import cors from 'cors'
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const redisClient = createClient();
+
+redisClient.on("error", (err) => console.log("Redis Client Error", err));
+
+app.post('/submit', async (req, res) => {
+    const { code, language, userId } = req.body;
+
+    const submissionId = `submission-${Date.now()}-${userId}}`
+    console.log(`Received submission from user ${userId}`);
+
+    try {
+        await redisClient.lPush("problems", JSON.stringify({ code, language, userId, submissionId }));
+
+        console.log(
+            `Submission pushed to Redis for: ${userId}  and problem id: ${submissionId}`
+        );
+
+        res.status(200).send("Submission received and stored");
+    } catch (err) {
+        console.log(err);
+        res.status(500).send("Failed to store submission");
+    }
+})
+
+const server = app.listen(3000, () => {
+    console.log("Express Server Listening on port 3000");
+});
+
+
+async function main() {
+    try {
+        await redisClient.connect();
+        console.log("Redis Client Connected");
+    } catch (error) {
+        console.log("Failed to connect to Redis", error);
+    }
+}
+
+
+main();
+
