b1c1597c07a21f0723e81dad0c741f8364685205
Ansh Tale <anshtale2004@gmail.com>
2025-05-31 13:58:41 +0530

    -completed Register page

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,27 +1,89 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useRecoilState } from 'recoil';
 import { userAtom } from '../atoms/userAtom';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
+import { socketAtom } from '../atoms/socketAtom';
 
 export const Register = () => {
     const [name, setName] = useState<string>("");
-    const [age, setAge] = useState<number>(0); 
-    const [, setUser] = useRecoilState(userAtom);
+    const [roomId,setRoomId] = useState<string>("");
+    const [loading,setLoading] = useState<boolean>(false);
+    const [socket,setSocket] = useRecoilState<WebSocket | null>(socketAtom);
+    const [user, setUser] = useRecoilState(userAtom);
+
+    const params = useParams();
     const navigate = useNavigate();
 
     const generateId = () => {
-        const id = Math.floor(Math.random() * 1000);
+        const id = Math.floor(Math.random() * 100000);
         return id.toString();
     }
 
-    const handleSubmit = () => {
-        setUser({
-            id: generateId(),
-            name: name,
-            age: age
-        });
-        navigate("/");
-    };
+    const initializeSocket = ()=>{
+        setLoading(true);
+        let generatedId = "";
+
+        if(user.id == ""){
+            generatedId = generateId();
+            setUser({
+                id: generatedId,
+                name: name,
+                roomId: ""
+            });
+        }
+
+        if(!socket || socket.readyState === WebSocket.CLOSED){
+            //protocol has been used to send userId in initial handshake b/w user and ws-server
+            const ws = new WebSocket(`ws://localhost:8080?roomId=${roomId}`,user.id == "" ? generateId() : user.id);
+
+            setSocket(ws);
+
+            ws.onopen = ()=>{
+                console.log("Connected to server");
+            }
+
+            ws.onmessage = (event)=>{
+                const data = JSON.parse(event.data);
+                if(data.type == "roomId"){
+                    setRoomId(data.roomId);
+                    console.log("Room ID: ",data.roomId);
+                    setUser({
+                        id: user.id == "" ? generateId() : user.id,
+                        name:name,
+                        roomId:data.roomId
+                    });
+                    setLoading(false);
+                    navigate("/code/" + data.roomId);
+                }
+            };
+
+            ws.onclose = ()=>{
+                console.log("WebSocket connnection closed from register page");
+
+                setLoading(false);
+            }
+        }else{
+            setLoading(false);
+        }
+    }
+
+    const handleNewRoom = ()=>{
+        if(!loading){
+            initializeSocket();
+        }
+    }
+
+    const handleJoinRoom = ()=>{
+        if(roomId != "" && roomId.length == 6 && !loading){
+            initializeSocket();
+        }else{
+            alert("Please enter a valid room ID");
+        }
+    }
+
+    useEffect(()=>{
+        setRoomId(params.roomId || "");
+    },[])
 
     return (
         <div className="flex flex-col items-center justify-center min-h-screen bg-gray-900 text-white">
@@ -38,17 +100,25 @@ export const Register = () => {
 
                 <input
                     type="number"
-                    placeholder="Age"
-                    value={age}
-                    onChange={(e) => setAge(parseInt(e.target.value))}
+                    placeholder="Room ID (Optional)"
+                    value={roomId}
+                    onChange={(e) => setRoomId(e.target.value)}
                     className="w-full p-3 bg-gray-700 rounded-lg text-white focus:outline-none focus:ring-2 focus:ring-blue-500 transition duration-200 mb-6"
                 />
 
                 <button
-                    onClick={handleSubmit}
+                    disabled = {loading}
+                    onClick={handleNewRoom}
                     className="w-full py-3 bg-gradient-to-r from-blue-500 to-blue-600 hover:from-blue-600 hover:to-blue-700 text-white rounded-lg font-semibold shadow-md hover:shadow-lg transition-transform duration-300 transform hover:scale-105"
                 >
-                    Submit
+                    Create New Room
+                </button>
+                <button
+                    disabled = {loading}
+                    onClick={handleJoinRoom}
+                    className="w-full py-3 bg-gradient-to-r from-green-500 to-green-600 hover:from-green-600 hover:to-green-700 text-white rounded-lg font-semibold shadow-md hover:shadow-lg transition-transform duration-300 transform hover:scale-105 mt-4"
+                >
+                    Join Room
                 </button>
             </div>
         </div>
